1842eeae78d2dcfd36455eb7edf33681ce7590f0
siddsharma25 <siddharthsharma25397@gmail.com>
2024-10-01 03:35:04 +0530

    using delimiters
    starting with comma

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -22,16 +22,40 @@ describe('HomePage', () => {
     expect(component).toBeTruthy();
   });
 
-  
-  it('should show sum of single character string "4" as 4', () => {
+  it('should show sum of empty string "" as 0', () => {
+    let str = ''; 
+    component.add(str);
+    expect(component.sum).toEqual(0); 
+   });
+
+  it('should show sum of single number string "4" as 4', () => {
     let str = '4'; 
     component.add(str);
     expect(component.sum).toEqual(4); 
    });
 
-   it('should show sum of empty string "" as 0', () => {
-    let str = ''; 
+   it('should show sum of two comma separated numbers  "1,4" as 5', () => {
+    let str = '1,4'; 
     component.add(str);
-    expect(component.sum).toEqual(0); 
+    expect(component.sum).toEqual(5); 
    });
+
+   it('should show sum of numbers separated in different lines  "1\n2,3" as 6', () => {
+    let str = "1\n2,3" ; 
+    component.add(str);
+    expect(component.sum).toEqual(6); 
+   });
+
+   it('should show sum of numbers separated by different delimiters  "//;\n1;2" as 3', () => {
+    let str = "//;\n1;2" ; 
+    component.add(str);
+    expect(component.sum).toEqual(3); 
+   });
+
+  //  it('should show sum of alphanumeric string "!2" as 2', () => {
+  //   let str = '!2'; 
+  //   component.add(str);
+  //   expect(component.sum).toEqual(2); 
+  //  });
+  
 });
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,8 +9,19 @@ export class HomePage {
   sum:number =0;
   constructor() {}
 
+  // add(str: any){
+  //   this.sum=0;
+  //   for(let i=0;i<str?.length;i++){
+  //     if(/\d/.test(str[i])){
+  //       this.sum += +str[i]
+  //     }
+  //   }
+    
+  // }
   add(str: any){
     this.sum=0;
+    str = str.split(',')
+    // console.log('%c [ str ]-24', 'font-size:13px; background:black; color:#e9ed18fa;', str)
     for(let i=0;i<str?.length;i++){
       if(/\d/.test(str[i])){
         this.sum += +str[i]
