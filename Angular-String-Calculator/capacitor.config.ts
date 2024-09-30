5adb049b8dd05bad7be6b4f70de0113be547c1a6
siddsharma25 <siddharthsharma25397@gmail.com>
2024-10-01 00:37:40 +0530

    Revert "Initial Commit"
    This reverts commit 6ef21e309075619bcd89b42b8f2c5124e44bf7cf.

--- Angular-String-Calculator/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Angular-String-Calculator',
-  webDir: 'www'
-};
-
-export default config;
