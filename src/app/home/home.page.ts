ded4fa26fb2b30399de1c3fcc612b7f31ac88bfc
siddsharma25 <siddharthsharma25397@gmail.com>
2024-10-01 12:38:55 +0530

    functions written to cover all cases and theirtest cases.
    functions written to cover all cases and their test cases.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,25 +9,60 @@ export class HomePage {
   sum:number =0;
   constructor() {}
 
+  // function to calculate sum for simple string with comma separated numbers 
   // add(str: any){
-  //   this.sum=0;
-  //   for(let i=0;i<str?.length;i++){
-  //     if(/\d/.test(str[i])){
-  //       this.sum += +str[i]
+    
+  //     this.sum=0;
+  //     str = str.split(',')
+  //     for(let i=0;i<str?.length;i++){
+       
+  //           this.sum += +str[i]
   //     }
-  //   }
-    
+   
   // }
+
+  // function to calculate sum for numbers string separated in different lines 
+  // add(str: any){
+  //     this.sum=0;
+  //     str = str.split('\n')
+  //     let temp=[];
+  //     for(let item of str){
+  //       temp.push(item.split(','));
+  //     }
+      
+  //     for(let i=0;i<temp?.length;i++){
+  //       for(let j=0; j<temp[i]?.length; j++){
+
+  //         this.sum += +temp[i][j]
+      
+  //       }
+  //   }
+   
+  // }
+
+  //function to calculate sum for numbers string with custom delimiters
   add(str: any){
-    this.sum=0;
-    str = str.split(',')
-    // console.log('%c [ str ]-24', 'font-size:13px; background:black; color:#e9ed18fa;', str)
-    for(let i=0;i<str?.length;i++){
-      if(/\d/.test(str[i])){
-        this.sum += +str[i]
-      }
-    }
     
-  }
+      this.sum=0;
+      let negatives='';
+      str = str.split('\n')
+      let delimiter = str[0];
+      str = str[1];
+      str = str.split(delimiter);
+      for(let i=0;i<str?.length;i++){
+       if(str[i]<0){        
+            negatives += str[i]+',';
+          } else if(negatives?.length==0){
+            this.sum += +str[i]
+          }
+                
+      }
+      if(negatives.length){
+        this.sum=0;
+        throw new Error('negative numbers not allowed '+ negatives.slice(0,negatives.length-1))
+      }
+    
+   }
+
 }
 
